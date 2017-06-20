6487a20330db3603e0aee24def6c62cec3db1a88
itay <itay@compedia.net>
2017-06-20 18:03:42 +0300

    poc for the soundcloud api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 require('./index.html')
+
+import SC from 'soundcloud';
 import angular from 'angular';
+import ngRedux from 'ng-redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers';
+
+
 import leftSide from './components/left-side/leftSide';
 
-angular.module('galacoral',[leftSide])
+angular.module('galacoral',[ngRedux,leftSide])
+  .constant('clientId','ggX0UomnLs0VmW7qZnCzw')
+  .config(($ngReduxProvider) => {
+      var state = {
+          counter: 1
+      };
+
+      //Creates the Redux store, and allow connect() to access it.
+      $ngReduxProvider.createStoreWith(rootReducer, [thunk], [],state);
+  })
+  .run(()=> {
+    SC.initialize({
+      client_id: 'ggX0UomnLs0VmW7qZnCzw'
+    })
+    SC.get('/tracks', {
+      q: 'streetlights'
+    }).then((tracks) => {
+
+      console.log('tracks',tracks);
+    });
+  })
 
 
 //kick start angular on doc ready.
---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by itay on 08/11/16.
+ */
+import { combineReducers, applyMiddleware } from 'redux';
+
+const rootReducers = combineReducers({
+
+})
+// var rootReducers = reduceReducers(
+//   undoRedo,counter,stam
+// )
+
+
+export default rootReducers;
